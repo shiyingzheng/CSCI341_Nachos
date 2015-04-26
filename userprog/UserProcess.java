fbbfc70baba35940ebdeeca4086b6e6d08dbedda
SHIYING <szheng@oberlin.edu>
2015-04-26 00:39:59 -0400

    how about now?

--- userprog/UserProcess.java
+++ userprog/UserProcess.java
@@ -592,8 +592,6 @@ public class UserProcess {
       return -1;
     }
 
-    System.out.println("Lychee says hi");
-
     int argc = a1;
     String[] args = new String[argc]; 
 
@@ -601,6 +599,7 @@ public class UserProcess {
       byte[] argPointer = new byte[4];
       int pointerLength = readVirtualMemory(a2 + i*4, argPointer, 0, 4);
       if (pointerLength == 0){
+        System.out.println("Lychee says hi");
         return -1;
       }
       String arg = readVirtualMemoryString(Lib.bytesToInt(argPointer, 0), 256);
